c0c6c411581f6880d2d5ccb60824574a628ba5fd
Bjørn Harbo Dahle <bdahle@hey.com>
2021-04-07 09:09:23 +0200

    add play round logid

--- main.js
+++ main.js
@@ -2,8 +2,32 @@
 let computerPlay = () => {
   const options = ["Rock", "Paper", "Scissors"];
   const random = Math.floor(Math.random() * options.length);
-  console.log(random, options[random]);
   return options[random];
 };
 
-computerPlay();
+// take two parameters
+let playRound = (playerSelection, computerSelection) => {
+  playerSelection = playerSelection.toUpperCase();
+  computerSelection = computerSelection.toUpperCase();
+  console.log("Player: " + playerSelection);
+  console.log("Computer: " + computerSelection);
+  // compare player selection and computer selection
+  if (playerSelection === computerSelection) {
+    return "Draw!";
+  }
+  let winner;
+  switch (playerSelection) {
+    case "ROCK":
+      winner = computerSelection === "PAPER" ? "Computer" : "Player";
+      break;
+    case "PAPER":
+      winner = computerSelection === "SCISSORS" ? "Computer" : "Player";
+      break;
+    case "SCISSORS":
+      winner = computerSelection === "ROCK" ? "Computer" : "Player";
+      break;
+  }
+  return winner + " won!";
+};
+
+console.log(playRound("PAPer", computerPlay()));
